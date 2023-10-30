5bbee9b7dc22657dd3d05b9c0fd24e8ca0468478
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-10-30 23:26:01 +0300

    Add project files.

---
+++ IdentityInveonExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityInveonExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityInveonExample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ IdentityInveonExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityInveonExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
